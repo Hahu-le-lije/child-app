4f5edc8817b3bff7bc7bf6e005e2b412b97c697a
Izzat-Engida <izzatengida7@gmail.com>
2026-03-03 18:16:21 +0300

    minor layout files

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const ScreenLayout = () => {
+  return (
+    <Stack screenOptions={{headerShown:false}}>
+
+    </Stack>
+  )
+}
+
+export default ScreenLayout
