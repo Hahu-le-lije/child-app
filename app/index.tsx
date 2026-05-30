8c9d5d8721b61f3f61752f429c30844c6c9acdbb
Izzat-Engida <izzatengida7@gmail.com>
2026-05-30 12:39:01 +0300

    s

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,10 @@ import { useAuthStore } from "@/store/authStore";
 
 export default function Index() {
   const { user, loading } = useAuthStore();
-
+console.log("ANALYSIS:", process.env.EXPO_PUBLIC_API_ANALYISIS);
+console.log("CHILD:", process.env.EXPO_PUBLIC_API_CHILD);
+console.log("SUB:", process.env.EXPO_PUBLIC_API_SUB);
+console.log("SYNC:", process.env.EXPO_PUBLIC_API_SYNC);
   if (loading) return null;
 
   if (user) {
